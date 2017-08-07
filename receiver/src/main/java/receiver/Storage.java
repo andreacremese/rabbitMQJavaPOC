fb1d6fccc1f6e1ba7356636efa5216dc11aa3e28
Andrea Cremese <cremese@gmail.com>
2017-08-07 13:36:10 -0700

    updated with memcached

--- receiver/src/main/java/receiver/Storage.java
+++ receiver/src/main/java/receiver/Storage.java
@@ -1,22 +1,30 @@
 package receiver;
 
 import messages.UpdateChacheMessage;
+import net.spy.memcached.MemcachedClient;
 
+import java.net.InetSocketAddress;
 import java.util.LinkedList;
 
 public class Storage {
 
-    public LinkedList<UpdateChacheMessage> list;
+    private MemcachedClient _mcc;
 
     public Storage() {
-        list = new LinkedList<UpdateChacheMessage>();
+
+        try{
+            // Connecting to Memcached server on localhost
+            _mcc = new MemcachedClient(new InetSocketAddress("127.0.0.1", 11211));
+            System.out.println("Connection to server sucessful.");
+
+
+        }catch(Exception ex){
+            System.out.println( ex.getMessage() );
+        }
     }
 
     public void add(UpdateChacheMessage msg) {
-        if (list == null) {
-            list = new LinkedList<UpdateChacheMessage>();
-        }
-        list.add(msg);
+        // now set data into memcached server
+        _mcc.set(msg.key, 900, msg.value);
     }
-
 }
